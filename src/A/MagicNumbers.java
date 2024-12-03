37db7188dd63292eeb94ce7247bc2b81188a5207
Ayush_Swar <aayush.swar.3@gmail.com>
2024-12-03 20:33:11 +0545

    Add early exit if first element is zero

--- src/A/MagicNumbers.java
+++ src/A/MagicNumbers.java
@@ -8,6 +8,7 @@ public class Main {
         int sum = 0;
         
         for(int i=0; i < a.length; i++) {
+            if(a[0] == 0) return 0;
             if(isPrime(a[i])){
                 sum += a[i];
             }
