eb1a6f4053c294fc13de3489d0387989ed2118ea
Ayush Swar <ayush@MacBook.local>
2024-12-04 09:48:49 +0545

    added question

--- src/A/MagicNumbers.java
+++ src/A/MagicNumbers.java
@@ -1,4 +1,15 @@
-public class Main {
+package A;
+/*
+2. An array is defined to be a Magic array if the sum of the primes in the array is equal to the first element of the array.
+If there are no primes in the array, the first element must be 0.
+So {21, 3, 7, 9, 11 4, 6} is a Magic array because 3, 7, 11 are the primes in the array and they sum to 21 which is the first element of the array.
+{13, 4, 4, 4, 4} is also a Magic array because the sum of the primes is 13 which is also the first element.
+Other Magic arrays are {10, 5, 5}, {0, 6, 8, 20} and {3}.
+{8, 5, -5, 5, 3} is not a Magic array because the sum of the primes is 5+5+3 = 13.
+Note that -5 is not a prime because prime numbers are positive.
+*/
+
+public class MagicNumbers {
     public static void main(String[] args) {
           int[] arr = {21, 3, 7, 9, 11, 4, 6};
         System.out.print(isMagicArray(arr));
@@ -6,11 +17,11 @@ public class Main {
     
     public static int isMagicArray(int[] a) {
         int sum = 0;
-        
-        for(int i=0; i < a.length; i++) {
-            if(a[0] == 0) return 0;
-            if(isPrime(a[i])){
-                sum += a[i];
+
+        for (int j : a) {
+            if (a[0] == 0) return 0;
+            if (isPrime(j)) {
+                sum += j;
             }
         }
         return (sum == a[0]) ?1 : 0;
